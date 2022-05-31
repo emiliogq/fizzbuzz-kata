89ad9e9428c9a11fb4af5d63c715a70b22a8f96b
Emilio Garcia <github@emiliogq.com>
2022-05-31 09:39:12 +0200

    Second TDD iteration
    Green stage. Solution implemented

--- src/main/java/com/codurance/academy/tdd/tdd_intro/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/codurance/academy/tdd/tdd_intro/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,9 @@ package com.codurance.academy.tdd.tdd_intro.fizzbuzz;
 
 public class FizzBuzz {
     public String getString(int i) {
+        if (i == 5){
+            return "Buzz";
+        }
         return "Fizz";
     }
 }
