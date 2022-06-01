8097de6044b5ea44218e65d1c2cebbea39c8148a
Emilio Garcia <github@emiliogq.com>
2022-06-01 07:42:02 +0200

    Sixth TDD iteration. Refactor stage
    Because of last change, the multiple of 3 and 5 test case fails
    Make it pass

--- src/main/java/com/codurance/academy/tdd/tdd_intro/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/codurance/academy/tdd/tdd_intro/fizzbuzz/FizzBuzz.java
@@ -3,14 +3,17 @@ package com.codurance.academy.tdd.tdd_intro.fizzbuzz;
 public class FizzBuzz {
     public String getString(int i) {
         String result = ""+i;
-        if (i == 15){
+        if (i % 3 == 0 && i % 5 == 0){
             result = "FizzBuzz";
         }
-        if (i == 5){
-            result = "Buzz";
-        }
-        if (i % 3 == 0){
-            result = "Fizz";
+        else {
+            if (i % 3 == 0){
+                result = "Fizz";
+            }
+            if (i == 5){
+                result = "Buzz";
+            }
+
         }
 
         return result;
